0bc7ffc8a80743652500cc03b6d1a82c2cf2e40d
rnegrete <rnegrete@dpw.lacounty.gov>
2019-02-12 15:07:30 -0800

    refactor: update import path

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import "./styles/placeholder-styles.css";
 import "./styles/placeholder-widths.css";
-export { PlaceholderFallback, IPlaceholderFallbackProps } from "./PlaceholderFallback";
-export { TextBlock } from "./placeholders/TextBlock";
-export { ParagraphBlock } from "./placeholders/ParagraphBlock";
+export { PlaceholderFallback, IPlaceholderFallbackProps } from "./components/PlaceholderFallback";
+export { TextBlock, ParagraphBlock, Circle } from "./components/placeholders";
